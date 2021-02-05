e6c3c65ef73fe121d644cb56fda2036d64562f26
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2021-02-06 01:08:29 +0530

    Create 1. Singly LinkedList Node Creation.cpp

---
+++ 2. LinkedList/1. Singly LinkedList/1. Singly LinkedList Node Creation.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+	int data;
+	struct node *next;
+}node;
+
+node *createNode(){
+	node *temp;
+	temp=(node*) malloc (sizeof(node));
+	return temp;
+}
+
+int main(){	
+
+	node *head;
+	node *temp=createNode();
+	temp->data=100;
+	temp->next=NULL;
+	if(head==NULL)
+		head=temp;
+	
+	printf("Data: %d\n",temp->data);
+	
+	return 0;
+}
+
+
+/*
+*	Author: Soumyadip Chowdury
+*	Youtube: Println
+*	Github: Soumyadip007
+*/
