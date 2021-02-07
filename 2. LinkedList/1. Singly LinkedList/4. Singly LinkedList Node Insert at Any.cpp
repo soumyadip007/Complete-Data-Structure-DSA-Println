b2bf2083ab51d73ace5360f191a05e1e4cb36e23
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2021-02-07 23:41:59 +0530

    Create 4. Singly LinkedList Node Insert at Any.cpp

---
+++ 2. LinkedList/1. Singly LinkedList/4. Singly LinkedList Node Insert at Any.cpp
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+	int data;
+	struct node *next;
+}node;
+
+node *head=NULL;
+
+node *createNode(){
+	node *temp;
+	temp=(node*) malloc (sizeof(node));
+	return temp;
+}
+
+void insert(){	
+	node *temp=createNode();
+	printf("\tEnter the data : ");
+	scanf("%d",&temp->data); //take data
+	temp->next=NULL;
+	if(head==NULL)
+		head=temp;
+	else{
+		node *ref=head;
+		while(ref->next != NULL)
+			ref=ref->next;
+		ref->next=temp;  //storing
+	
+	}
+	printf("\tSuccessfully inserted Data :)\n");
+}
+
+
+void insertAtAny(){	
+	node *temp=createNode();
+	printf("\tEnter the data (for a specific position): ");
+	scanf("%d",&temp->data); //take data
+	temp->next=NULL;
+	int pos; 
+	printf("\tEnter the Position : ");
+	scanf("%d",&pos); //take position
+	int count=1;	
+	node *t=head, *prev;
+	if(pos<=0 || (pos>count  && head==NULL))
+		printf("\tInvalid\n");
+	else if(pos==1){
+		temp->next=head;
+	    head=temp;
+	}
+	else{
+		while(t->next!=NULL && count<pos){
+			prev=t;
+			t=t->next;
+			count++;
+		}
+		if(pos>count) {
+			printf("\tInvalid\n");
+		}	
+		else{
+			prev->next=temp;   
+			temp->next=t;	
+		}
+	}	
+}
+
+
+void traverse(){
+	node *ref=head;
+	printf("\tData present in the List: \n");
+	while(ref != NULL)
+	{
+		printf("\t%d\n", ref->data);
+		ref=ref->next;
+	}	
+}
+
+int main(){	
+
+	insert();
+	insert();
+	insert();
+	insert();
+	
+	
+	insertAtAny();
+	traverse();
+	
+	insertAtAny();
+	traverse();
+	
+	insertAtAny();
+	traverse();
+	
+	insertAtAny();
+	traverse();
+	
+	return 0;
+}
+
+
+/*
+*	Author: Soumyadip Chowdury
+*	Youtube: Println
+*	Github: Soumyadip007
+*/
