39aa69cc238bf6fc03ac2a9ff3b1a22cfdba559a
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2021-02-08 23:52:46 +0530

    Create 1 Array Initialization.cpp

---
+++ 1. Array/1 Array Initialization.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+
+	int arr[5];
+	 
+	arr[0] = 100;
+	arr[1] = 200;
+	arr[2] = 300;
+	arr[3] = 400;
+	arr[4] = 500;
+
+	printf("\t%d\n",arr[0]);
+	printf("\t%d\n",arr[1]);
+	printf("\t%d\n",arr[2]);
+	printf("\t%d\n",arr[3]);
+	printf("\t%d\n",arr[4]);
+}
