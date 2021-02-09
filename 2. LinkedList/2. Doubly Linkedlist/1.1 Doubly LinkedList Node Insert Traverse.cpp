caf5f8c3089f018d322545126cde986f97f0c13f
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2021-02-09 22:47:16 +0530

    Create 1.1 Doubly LinkedList Node Insert Traverse.cpp

---
+++ 2. LinkedList/2. Doubly Linkedlist/1.1 Doubly LinkedList Node Insert Traverse.cpp
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+	int data;
+	struct node *next;
+	struct node *prev;
+}node;
+
+node *head=NULL;
+
+node *createNode(){
+	node *temp;
+	temp=(node*) malloc (sizeof(node));
+	return temp;
+}
+
+void insert(){	
+	node *temp=createNode();
+	printf("\n\tEnter the value: ");
+	scanf("%d",&temp->data); //take data
+	temp->prev=NULL;
+	temp->next=NULL;
+	if(head==NULL)
+		head=temp;
+	else{
+		node *ref=head;
+		while(ref->next != NULL)
+			ref=ref->next;
+		temp->prev=ref;
+		ref->next=temp;  //storing
+	
+	}
+}
+
+
+void traverse(){
+	if(head==NULL){
+		printf("\n\tNo data present in the List");
+		return;
+	}
+	node *ref=head;
+	while(ref != NULL)
+	{
+		printf("\t%d\n", ref->data);
+		ref=ref->next;
+	}	
+}
+
+
+int main(){	
+
+
+	insert();
+	insert();
+	insert();
+	insert();
+	insert();
+
+
+	traverse();
+	return 0;
+}
+
+
+/*
+*	Author: Soumyadip Chowdury
+*	Youtube: Println
+*	Github: Soumyadip007
+*/
