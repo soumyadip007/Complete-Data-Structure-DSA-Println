48c17567e85c20ba0df642941f202e2ff784bf25
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2021-04-07 21:36:39 +0530

    Create 2. POP.cpp

---
+++ 3. Stack/2. POP.cpp
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define MAX 5
+#define MIN -1
+int stack[MAX];
+int top = -1;
+
+bool notOverFlow()
+{
+    if (top + 1 < MAX )
+        return true;
+    return false;
+}
+
+bool notUnderFlow()
+{
+    if (top != MIN) //Min = -1
+        return true;
+    return false;
+}
+
+void printStack()
+{
+    if (notUnderFlow())
+    {
+        for (int i = 0; i <= top; i++)
+        {
+            printf("%d   ", stack[i]);
+        }
+    }
+    else
+    {
+        printf("\nSTACK UNDERFLOW\n");
+    }
+    printf(" \n\n");
+    return;
+}
+
+void pop()
+{
+    if (notUnderFlow()){
+    	printf("\nPopped value : %d\n",stack[top--]);
+	}
+    else
+    {
+        printf("\nSTACK UNDERFLOW\n");
+    }
+}
+
+void push(int value)
+{
+    if (notOverFlow())
+        stack[++top] = value;
+    else
+    {
+        printf("\nSTACK OVREFLOW\n");
+    };
+}
+
+int main()
+{
+    push(10);
+    printStack();
+    push(20);
+    printStack();
+    push(30);
+    printStack();
+    push(40);
+    printStack();
+    push(50);
+    printStack();
+    push(60);
+    printStack();
+    
+    pop();
+    printStack();
+    pop();
+    printStack();
+    pop();
+    printStack();
+    pop();
+    printStack();
+    pop();
+    printStack();
+    pop();
+    printStack();
+	pop();
+    
+    return 0;
+}
