42978cdefa64c45d362094ea61024530e48c9d07
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2021-04-07 21:36:25 +0530

    Update 4 Array Deletion at Any.cpp

--- 1. Array/4 Array Deletion at Any.cpp
+++ 1. Array/4 Array Deletion at Any.cpp
@@ -2,18 +2,29 @@
 int main(){
 	
 	int arr[100]={9, 19, 29, 39, 49, 59, 69,79};
-	int key=49, i=0, j=0, n=8 ;
-	
+	int key=109, i=0, j=0, n=8 ;
+	int found=0;
 	for(i=0;i<n;i++)			
-		if(arr[i]==key)
+		if(arr[i]==key){
+			found=1;
 			break;
+		}
+		
+	for(j=i; j<n-1; j++)	{
+			arr[j]=arr[j+1];
+	}	
 	
-	for(j=i; j<n-1; j++)			
-		arr[j]=arr[j+1];
-
-	for(int i=0;i<n-1;i++){
-		printf("\t%d\n",arr[i]);
+	if(found==1){
+		for(int i=0;i<n-1;i++){
+			printf("\t%d\n",arr[i]);
+		}
 	}
+	else{
+		for(int i=0;i<n;i++){
+			printf("\t%d\n",arr[i]);
+		}
+	}
+
     
 }
 
