7b4181335f0f8dff028f04abdfdbe1cd38846d80
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2021-04-07 21:38:03 +0530

    Update 1. Singly LinkedList Node Creation.cpp

--- 2. LinkedList/1. Singly LinkedList/1. Singly LinkedList Node Creation.cpp
+++ 2. LinkedList/1. Singly LinkedList/1. Singly LinkedList Node Creation.cpp
@@ -18,11 +18,16 @@ int main(){
 	node *temp=createNode();
 	temp->data=100;
 	temp->next=NULL;
-	if(head==NULL)
-		head=temp;
+	if(head==NULL){
+			head=temp;
+					
+			printf("Data: %d\n",temp->data);
+			printf("Head: %d\n",head);
+			printf("Temp: %d\n",temp);
+	}
 	
-	printf("Data: %d\n",temp->data);
 	
+
 	return 0;
 }
 
